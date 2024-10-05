6e28247c09ea42203d984a320250adb9011c486f
Till75 <tillseyfarth@gmail.com>
2024-10-05 14:43:06 -0600

    New clock practice

--- test/test_projectile.c
+++ test/test_projectile.c
@@ -5,6 +5,7 @@
 #include "projectile.h"
 #include "canvas.h"
 #include "color.h"
+#include "transforms.h"
 
 void setUp(void)
 {
@@ -101,6 +102,40 @@ void test_projectile_CanvasTickToFile(void)
     } while ((*projectile.position)[1] > 0.0);
 
     canvas_PixelsToPPMFile(&canvas, "image.ppm");
+    canvas_Destroy(&canvas);
+}
+
+void test_projectile_CircleTickToFile(void)
+{
+
+    Tuple4d pos = {0,0,0,1};
+    Matrix4d rot_z = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+    Matrix4d tr_1 = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+    transforms_GetTranslationMatrix4d(&tr_1, 0, 40, 0);
+    Matrix4d tr_2 = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+
+    Canvas canvas;
+    canvas_Create(&canvas, 100, 100);
+    Color red;
+    color_Create(&red, 1, 0, 0);
+
+    for (int i = 0; i < 12; i++)
+    {
+        pos[0] = 0; pos[1] = 0; // reset pos
+        transforms_GetRotationZMatrix4d(&rot_z, M_PI / 6 * (float)i); // new rot matrix for each tick
+        transforms_GetTranslationMatrix4d(&tr_2, 50, 50, 0); // reset tr_2
+
+        vecmath_MultiplyMatrix4d(&tr_2, &rot_z);
+        vecmath_MultiplyMatrix4d(&tr_2, &tr_1);
+        vecmath_MultiplyTuple4dByMatrix4d(&pos, &tr_2);
+
+        int x = (int)pos[0];
+        int y = (int)pos[1];
+        canvas_DrawPixel(&canvas, &red, x, canvas.height - y);
+
+    }
+
+    canvas_PixelsToPPMFile(&canvas, "circle.ppm");
     canvas_Destroy(&canvas);
 }
 
